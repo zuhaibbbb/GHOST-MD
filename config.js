36262ff708cafd6ec4a8b1df976ea3eb097be174
zuhaibbbb <tzohaib812@gmail.com>
2024-09-24 00:55:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GKBQgSoR#xshdD3sdWPrY3bUGP1XGFkdk3g2Hapb3q_Wie6_utKY",
+SESSION_ID: process.env.SESSION_ID || "9M4TUKhL#_KId_SMmES3Nc8wuVrxria6JlG-dRJEoOY7iUr-GOkQ",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
